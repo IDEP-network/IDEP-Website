0356b9dc15d7ab082854641d1768e395f08e0a7e
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-03-26 20:41:45 +0400

    :rocket: changed the swap section, give it a heading and icons with updated image, changed the card alignment on 13inch

--- components/pages/landing/uptake/Uptake.tsx
+++ components/pages/landing/uptake/Uptake.tsx
@@ -4,9 +4,8 @@ const Uptake = () => {
             <section className="uptake_main">
                 <div className="container">
                     <div className="content">
-                        <h1 className="f-size-h2 f-weight-bl">UPTAKE</h1>
-                        <p className="description f-size-p4 f-weight-l">
-                            <b>NFT 2.0 made effortlessly.</b> {" "}
+                        <h1 className="f-size-h2 f-weight-bl">UPTAKE <b>NFT 2.0 made effortlessly.</b></h1>
+                        <p className="description f-size-p3 f-weight-l">
                             The next-generation NFT creation platform with superpowers UPTAKE can
                             not only mint NFTs of any type of digital content, but it also allows
                             users to attach claims to their NFT 2.0 such as intellectual property
@@ -16,9 +15,20 @@ const Uptake = () => {
                             the intellectual rights themselves can be tokenized and split into any
                             percentage an individual may wish to use in a variety of ways
                         </p>
-                        <p className="f-size-p1 f-weight-r">Create data access tokens Mint NFTs</p>
-                        <p className="f-size-p1 f-weight-r">Tokenize digital content</p>
-                        <p className="f-size-p1 f-weight-r">Establish your Intellectual property right onchain</p>
+                        <div className="features_container">
+                            <div className="feature f-size-h6 f-weight-bo">
+                                <div className="icon" />
+                                <p>Create data access tokens Mint NFTs</p>
+                            </div>
+                            <div className="feature f-size-h6 f-weight-bo">
+                                <div className="icon" />
+                                <p>Tokenize digital content</p>
+                            </div>
+                            <div className="feature f-size-h6 f-weight-bo">
+                                <div className="icon" />
+                                <p>Establish your Intellectual property right onchain</p>
+                            </div>
+                        </div>
                         {/* <p className="f-size-p1 f-weight-r">Combine any of the four features into composable tokens creating custom financial asset classes.</p> */}
                     </div>
                     <div className="image" />
