19a0886e4789c1094d1a0eb74c364d77bf1e32d9
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-03-27 18:28:17 +0400

    :rocket: using dynamic for the animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 import { useEffect } from 'react';
 import gsap from "gsap";
 gsap.registerPlugin(ScrollTrigger)
@@ -10,13 +10,13 @@ import Navigation from 'components/global/navigation/Navigation';
 import Blockchain from 'components/pages/landing/blockchain/Blockchain';
 import Media from 'components/pages/landing/media/Media';
 import Newsletter from 'components/pages/landing/newsletter/Newsletter';
-import Swap from 'components/pages/landing/swap/Swap';
+// import Swap from 'components/pages/landing/swap/Swap';
 import Identt from 'components/pages/landing/identt/Identt';
 import Uptake from 'components/pages/landing/uptake/Uptake';
 import Hero from 'components/pages/landing/hero/Hero';
 import Software from 'components/pages/landing/software/Software';
 // const Hero = dynamic(() => import('components/pages/landing/hero/Hero'), { ssr: false })
-// const Swap = dynamic(() => import('components/pages/landing/swap/Swap'), { ssr: false })
+const Swap = dynamic(() => import('components/pages/landing/swap/Swap'), { ssr: false })
 
 
 
